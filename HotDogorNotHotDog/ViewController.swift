13ec8071a2c9ad17f979e71d6443613c547d19cb
Milton Casiano <90805282+MiltonCasiano88@users.noreply.github.com>
2021-11-29 20:07:52 -0500

    Initial Commit

---
+++ HotDogorNotHotDog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotDogorNotHotDog
+//
+//  Created by Milton Casiano on 11/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
