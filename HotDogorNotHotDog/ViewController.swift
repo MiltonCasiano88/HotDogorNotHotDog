f23814f7f530adcb9d18591e6c47041f8c5a8861
Milton Casiano <90805282+MiltonCasiano88@users.noreply.github.com>
2021-12-01 21:42:22 -0500

    Committed

--- HotDogorNotHotDog/ViewController.swift
+++ HotDogorNotHotDog/ViewController.swift
@@ -10,7 +10,7 @@ import CoreML
 import Vision
 
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     
     
@@ -20,32 +20,95 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         // Steps to implement a delegate:
         // 1. Create an object
     let imagePicker = UIImagePickerController()
-    let results = [VNClassificationObservation]
+    
+    var result: [VNClassificationObservation] = []
     
     //[0.8, 0.4, 0.3]
     
     @IBOutlet weak var imageView: UIImageView!
     
+    
+    
         //2. Initialize the delegate in View Did Load
     override func viewDidLoad() {
-      super.viewDidLoad()
+        super.viewDidLoad()
+        
         imagePicker.delegate = self
         
-        
-        
-        
     }
-    
-    
     
         //3. Implement its functions
     
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
         
         imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
-    func imagePickerController(_ picker:)
-}
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:
+                               [UIImagePickerController.InfoKey : Any]) {
 
+
+        if let image = info[.originalImage] as? UIImage {
+            
+            imageView.image = image
+            
+            imagePicker.dismiss(animated: true , completion : nil)
+            
+            guard let ciImage = CIImage(image : image) else {return}
+            
+            detect(image: ciImage)
+    }
+
+
+        func detect(image : CIImage) {
+
+
+            if let model = try? VNCoreMLModel(for : Inceptionv3().model) {
+                
+                // request the result
+                
+                let request = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+                            
+
+                        // Results -> (0.8, 0.7, 0.3) -> 0.8: Hot Dog
+                        guard let results = request.results as? [VNClassificationObservation], let topResult = results.first   else {return}
+                        
+                        
+                        if topResult.identifier.contains("hotdog") {
+                            //Main Thread   -   | | | |  | | |
+                            DispatchQueue.main.async {
+                                self.navigationItem.title = "Hotdog"
+                                
+                            }
+                        }
+                        
+                        else {
+                            
+                            
+                            DispatchQueue.main.async {
+                                self.navigationItem.title = " Not Hotdog"
+                                
+                            }
+                        }
+                    
+                    
+                })
+        
+        // handler
+        let handler = VNImageRequestHandler(ciImage : image)
+                    do {
+                        try handler.perform([request])
+                    }
+                    
+                    catch {
+                        
+                        print(error)
+                    }
+    }
+}
+}
+}
